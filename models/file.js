89ed8d923c90109ba1cb715ae0c6667f90913b53
jeevan20 <sjeevankumarjk@gmail.com>
2022-10-19 18:15:13 +0530

    added original name feild in db

--- models/file.js
+++ models/file.js
@@ -4,11 +4,13 @@ const Schema = mongoose.Schema;
 const fileSchema = new Schema(
   {
     filename: { type: String, required: true },
+    originalname: { type: String, required: true },
     path: { type: String, required: true },
     size: { type: Number, required: true },
     uuid: { type: String, required: true },
     sender: { type: String, required: false },
     receiver: { type: String, required: false },
+    password: { type: String, required: false },
   },
   { timestamps: true }
 );
